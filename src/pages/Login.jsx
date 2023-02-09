aee08f9eaf35e8acea2847cf64c451015baa2513
andreresende36 <andreresende36@gmail.com>
2023-02-09 18:07:47 -0300

    Requisito 2: ao apertar em play, salvo o token no localStorage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setNameAndEmail } from '../redux/actions';
+import getToken from '../services/apiTrivia';
 
 // A pessoa que joga deve conseguir escrever seu nome no input de texto
 // A pessoa que joga deve conseguir escrever seu email no input de email
@@ -42,8 +43,14 @@ class Login extends Component {
     });
   };
 
+  handlePlayButton = () => {
+    const { dispatch, history } = this.props;
+    dispatch(setNameAndEmail({ email: userEmail, name: userName }));
+    this.clearInputs();
+    getToken().then(history.push('/game'));
+  };
+
   render() {
-    const { dispatch } = this.props;
     const { userEmail, userName, isDisabled } = this.state;
     return (
       <form>
@@ -73,10 +80,7 @@ class Login extends Component {
         <button
           type="button"
           disabled={ isDisabled }
-          onClick={ () => {
-            dispatch(setNameAndEmail({ email: userEmail, name: userName }));
-            this.clearInputs();
-          } }
+          onClick={ this.handlePlayButton }
           data-testid="btn-play"
         >
           Play
@@ -88,6 +92,9 @@ class Login extends Component {
 
 Login.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default connect()(Login);
---
+++ src/services/apiTrivia.js
@@ -0,0 +1,9 @@
+const TOKEN_URL = 'https://opentdb.com/api_token.php?command=request';
+
+const getToken = async () => {
+  const response = await fetch(TOKEN_URL);
+  const data = await response.json();
+  localStorage.setItem('token', data.token);
+};
+
+export default getToken;

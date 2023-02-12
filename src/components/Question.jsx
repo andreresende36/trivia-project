b9f066754fdade40244a4cea920c9ef27c5e5afd
andreresende36 <andreresende36@gmail.com>
2023-02-12 10:19:28 -0300

    Requisitos de 9 a 12: implementação parcial. Aguardando requisitos 7 e 8

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,45 +3,77 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { changeIndexOfQuestions, increaseScore } from '../redux/actions';
 import { calcDifficultyIndex } from '../services/calcDifficultyIndex';
+import { randomAnswers } from '../services/randomAnswers';
 
-const sortNumber = 0.5;
 class Question extends Component {
-  handleAnswer = ({ target: { name } }) => {
-    const answer = name;
+  state = {
+    isAnswered: false,
+    arrayAnswers: [],
+  };
+
+  componentDidMount() {
+    const { questionData } = this.props;
+    const { correctAnswer, incorrectAnswers } = questionData;
+    this.setState({ arrayAnswers: randomAnswers(correctAnswer, incorrectAnswers) });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { questionData } = this.props;
+    const { correctAnswer, incorrectAnswers } = questionData;
+    if (questionData !== prevProps.questionData) {
+      this.setState({ arrayAnswers: randomAnswers(correctAnswer, incorrectAnswers) });
+    }
+  }
+
+  handleAnswer = ({ target: { name: answer } }) => {
     const { dispatch, questionData: { correctAnswer, difficulty } } = this.props;
     const difficultyIndex = calcDifficultyIndex(difficulty);
-    dispatch(changeIndexOfQuestions());
+    this.setState({ isAnswered: true });
     if (answer === correctAnswer) {
       dispatch(increaseScore(difficultyIndex));
     }
   };
 
+  handleNextQuestion = () => {
+    const { dispatch } = this.props;
+    this.setState({ isAnswered: false });
+    dispatch(changeIndexOfQuestions());
+  };
+
   render() {
     const { questionData } = this.props;
-    const { category, question, correctAnswer, incorrectAnswers } = questionData;
+    const { isAnswered, arrayAnswers } = this.state;
+    const { category, question, correctAnswer } = questionData;
+    const nextButton = (
+      <button
+        type="button"
+        onClick={ this.handleNextQuestion }
+        data-testid="btn-next"
+      >
+        Next question
+      </button>);
     return (
       <div>
         <h2 data-testid="question-category">{category}</h2>
         <h4 data-testid="question-text">{question}</h4>
         <div data-testid="answer-options">
-          {[correctAnswer, ...incorrectAnswers]
-            .sort(() => sortNumber - Math.random())
-            .map((answer, index) => (
-              <button
-                type="button"
-                key={ answer }
-                data-testid={
-                  answer !== correctAnswer
-                    ? `wrong-answer-${index}`
-                    : 'correct-answer'
-                }
-                name={ answer }
-                onClick={ this.handleAnswer }
-              >
-                {answer}
-              </button>
-            ))}
+          {arrayAnswers.map((answer, index) => (
+            <button
+              type="button"
+              key={ answer }
+              data-testid={
+                answer !== correctAnswer
+                  ? `wrong-answer-${index}`
+                  : 'correct-answer'
+              }
+              name={ answer }
+              onClick={ this.handleAnswer }
+            >
+              {answer}
+            </button>
+          ))}
         </div>
+        { isAnswered ? nextButton : null}
       </div>
     );
   }

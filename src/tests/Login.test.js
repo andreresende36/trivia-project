8dc8d0539b1ca83bc60155bc24a2063ef11671f0
guilherme <ghma.em1@gmail.com>
2023-02-13 16:42:35 -0300

    Criado testes para página Game e Login

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,8 +1,60 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+
+const token =
+  '9ec95505e16ba20ff6365430bebb4e4544b4f3695ddeb75a30e13cc2ba4e7115';
+const questions = {
+  response_code: 0,
+  results: [
+    {
+      category: 'Science & Nature',
+      type: 'boolean',
+      difficulty: 'easy',
+      question: 'Igneous rocks are formed by excessive heat and pressure.',
+      correct_answer: 'False',
+      incorrect_answers: ['True'],
+    },
+    {
+      category: 'Entertainment: Video Games',
+      type: 'multiple',
+      difficulty: 'medium',
+      question:
+        'Which of the following created and directed the Katamari Damacy series?',
+      correct_answer: 'Keita Takahashi',
+      incorrect_answers: ['Hideki Kamiya', 'Shu Takumi', 'Shinji Mikami'],
+    },
+    {
+      category: 'Entertainment: Japanese Anime & Manga',
+      type: 'multiple',
+      difficulty: 'hard',
+      question:
+        'What year was &quot;JoJo&#039;s Bizarre Adventure: Phantom Blood&quot; first released?',
+      correct_answer: '1987',
+      incorrect_answers: ['2013', '1983', '1995'],
+    },
+    {
+      category: 'Entertainment: Comics',
+      type: 'multiple',
+      difficulty: 'medium',
+      question: 'Who authored The Adventures of Tintin?',
+      correct_answer: 'Herg&eacute;',
+      incorrect_answers: ['E.P. Jacobs', 'Rin Tin Tin', 'Chic Young'],
+    },
+    {
+      category: 'Entertainment: Cartoon & Animations',
+      type: 'multiple',
+      difficulty: 'medium',
+      question:
+        'What is Dipper&#039;s real name from &quot;Gravity Falls&quot;?',
+      correct_answer: 'Mason Pines',
+      incorrect_answers: ['Mable Pines', 'Jason Pines', 'Mark Pines'],
+    },
+  ],
+};
 
 describe('Verifica campos da pagina inicial', () => {
   const validEmail = 'anfitrao@0413.com';
@@ -21,7 +73,7 @@ describe('Verifica campos da pagina inicial', () => {
     expect(inputEmail).toBeInTheDocument();
     expect(inputName).toBeInTheDocument();
     expect(button).toBeInTheDocument();
-    expect(button.disabled).toBe(true);
+    expect(button).toBeDisabled();
   });
 
   test('botão habilita em caso de ter ao mesmo tempo um email e um nome válidos', () => {
@@ -61,6 +113,11 @@ describe('Verifica campos da pagina inicial', () => {
   });
 
   test('Testa rota correta do usuario', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValueOnce(token).mockResolvedValue(questions),
+    });
+  
     const { history, store } = renderWithRouterAndRedux(<App />);
     const inputName = screen.getByTestId(TestIdName);
     const inputEmail = screen.getByTestId(TestIdEmail);
@@ -69,17 +126,91 @@ describe('Verifica campos da pagina inicial', () => {
     expect(inputEmail).toBeInTheDocument();
     expect(inputName).toBeInTheDocument();
     expect(button).toBeInTheDocument();
-    expect(button.disabled).toBe(true);
+    expect(button).toBeDisabled();
 
     userEvent.type(inputName, validName);
     userEvent.type(inputEmail, validEmail);
     expect(inputEmail).toHaveValue(validEmail);
     expect(inputName).toHaveValue(validName);
+    expect(button).not.toBeDisabled();
 
     userEvent.click(button);
-    expect(store.getState().player.name).toEqual(validName);
-    expect(store.getState().player.gravatarEmail).toEqual(validEmail);
     const { pathname } = history.location;
-    expect(pathname).toEqual('/game');
+    
+    const myStore = {
+      player: {
+        name: validName,
+        assertions: 0,
+        score: 0,
+        gravatarEmail: validEmail,
+      },
+      questions: {
+        reload: false,
+        apiQuestions: questions.results,
+        errorMessage: '',
+        indexOfCurrentQuestion: 0
+      }
+    }
+
+    expect(store.getState().player.name).toEqual(myStore.player.name);
+    expect(store.getState().player.gravatarEmail).toEqual(myStore.player.gravatarEmail);
+    await waitFor(() => {
+      expect(store.getState().questions).toEqual(myStore.questions);
+      expect(screen.getByTestId('question-category')).toBeInTheDocument();
+    });
+
   });
+
+  it('Testa rota para Settings', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const buttonSetting = screen.getByTestId('btn-settings');
+    fireEvent.click(buttonSetting);
+
+    const { pathname } = history.location;
+    expect(pathname).toEqual('/settings');
+
+    expect(screen.getByTestId('settings-title')).toBeInTheDocument();
+  });
+
+  it('Testa quando a api Questions retorna um erro', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValueOnce(token).mockRejectedValueOnce({
+        responseCode: 3,
+        results: [],
+      }),
+    });
+  
+    const { history, store } = renderWithRouterAndRedux(<App />);
+    const inputName = screen.getByTestId(TestIdName);
+    const inputEmail = screen.getByTestId(TestIdEmail);
+    const button = screen.getByRole('button', { name: /Play/i });
+
+    userEvent.type(inputName, validName);
+    userEvent.type(inputEmail, validEmail);
+    userEvent.click(button);
+
+    // const { pathname } = history.location;
+    
+    const myStore = {
+      player: {
+        name: validName,
+        assertions: 0,
+        score: 0,
+        gravatarEmail: validEmail,
+      },
+      questions: {
+        reload: true,
+        apiQuestions: [],
+        errorMessage: 'Token Inválido'
+      }
+    }
+
+    expect(store.getState().player.name).toEqual(myStore.player.name);
+    expect(store.getState().player.gravatarEmail).toEqual(myStore.player.gravatarEmail);
+    await waitFor(() => {
+      expect(store.getState().questions).toEqual(myStore.questions);
+      expect(screen.queryByTestId('question-category')).not.toBeInTheDocument();
+    });
+  })
 });

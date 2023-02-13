ace65fe7b9e5cb497a956c3751e3a4e340b6713b
Ismael Soares <srs.ismael@gmail.com>
2023-02-13 19:15:55 -0300

    Req 19 main ranking page

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import UserRanking from '../components/UserRanking';
+import { clearGlobalState } from '../redux/actions';
 
 class Ranking extends Component {
+  state = {
+    ranking: [],
+  };
+
+  componentDidMount() {
+    const ranking = JSON.parse(localStorage.getItem('Ranking'));
+    const orderedRanking = ranking.sort((a, b) => b.score - a.score);
+    this.setState({
+      ranking: orderedRanking,
+    });
+  }
+
   render() {
+    const { ranking } = this.state;
+    const { dispatch } = this.props;
     return (
       <div>
         <span data-testid="ranking-title">Ranking</span>
@@ -10,13 +28,30 @@ class Ranking extends Component {
           <button
             type="button"
             data-testid="btn-go-home"
+            onClick={ () => dispatch(clearGlobalState()) }
           >
             Login
           </button>
         </Link>
+        {ranking.map((item, index) => (
+          <UserRanking
+            index={ index }
+            key={ item.name }
+            name={ item.name }
+            score={ item.score }
+            gravatarEmail={ item.gravatarEmail }
+          />
+        ))}
       </div>
     );
   }
 }
+const mapStateToProps = (globalState) => ({
+  ...globalState.player,
+});
 
-export default Ranking;
+Ranking.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(Ranking);
